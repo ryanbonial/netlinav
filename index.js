31dd3a6bbd92552fab3cac4094ab8e33498deca1
Ryan Bonial <ryanbonial@gmail.com>
2020-09-06 19:25:14 -0600

    Refactors index.js

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-var inputs = document.getElementsByClassName('mode-switcher');
-
-for (var i = 0; i < inputs.length; i++) {
-  var el = inputs[i];
-  el.addEventListener('change', function() {
-    defineSetting(this.value);
-  })
+const navLevelRadios = document.getElementsByClassName('mode-switcher');
+for (const levelChoice of navLevelRadios) {
+  levelChoice.addEventListener('change', () => switchNavLevel(levelChoice.value));
 }
 
-function defineSetting(setting) {
-  if (setting == 'root') {
-    document.getElementById("nav1").classList.remove('hidden');
-    document.getElementById("nav2").classList.add('hidden');
-    document.getElementById("nav3").classList.add('hidden');
-    document.getElementById("sitebar").classList.add('hidden');
-    document.getElementById("title").classList.add('hidden');
-  } else if (setting == 'team') {
-    document.getElementById("nav1").classList.add('hidden');
-    document.getElementById("nav2").classList.remove('hidden');
-    document.getElementById("nav3").classList.add('hidden');
-    document.getElementById("sitebar").classList.add('hidden');
-    document.getElementById("title").classList.add('hidden');
-  } else if (setting == 'site') {
-    document.getElementById("nav1").classList.add('hidden');
-    document.getElementById("nav2").classList.add('hidden');
-    document.getElementById("nav3").classList.remove('hidden');
-    document.getElementById("sitebar").classList.remove('hidden');
-    document.getElementById("title").classList.remove('hidden');
+function switchNavLevel(level) {
+  // Reset by hiding all nav items
+  [...document.querySelectorAll('#nav1, #nav2, #nav3, #sitebar, #title')].forEach(el => el.classList.add('hidden'))
+  // Show nav for chosen nav level
+  if (level === 'root') {
+    document.querySelector('#nav1').classList.remove('hidden')
+  } else if (level === 'team') {
+    document.querySelector('#nav2').classList.remove('hidden')
+  } else if (level === 'site') {
+    [...document.querySelectorAll('#nav3, #sitebar, #title')].forEach(el => el.classList.remove('hidden'))
   }
 }
