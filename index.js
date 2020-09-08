871db3f83f42d3a367420a54648ff24903ce3509
Ryan Bonial <ryanbonial@gmail.com>
2020-09-07 20:24:17 -0600

    Removes h5 as links and provides meaningful names

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ for (const levelChoice of navLevelRadios) {
 
 function switchNavLevel(level) {
   // Reset by hiding all nav items
-  [...document.querySelectorAll('.nav1, .nav2, .nav3, #sitebar, #title')].forEach(el => el.classList.add('hidden'))
+  [...document.querySelectorAll('.nav-root, .nav-team, .nav-site, #sitebar, #title')].forEach(el => el.classList.add('hidden'))
   // Show nav for chosen nav level
   if (level === 'root') {
-    [...document.querySelectorAll('.nav1')].forEach(el => el.classList.remove('hidden'))
+    [...document.querySelectorAll('.nav-root')].forEach(el => el.classList.remove('hidden'))
   } else if (level === 'team') {
-    [...document.querySelectorAll('.nav2')].forEach(el => el.classList.remove('hidden'))
+    [...document.querySelectorAll('.nav-team')].forEach(el => el.classList.remove('hidden'))
   } else if (level === 'site') {
-    [...document.querySelectorAll('.nav3, #sitebar, #title')].forEach(el => el.classList.remove('hidden'))
+    [...document.querySelectorAll('.nav-site, #sitebar, #title')].forEach(el => el.classList.remove('hidden'))
   }
 }
